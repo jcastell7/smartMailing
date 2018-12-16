5fc97a572db2bf0e9a333412e35268b177dc0a06
juan tomas castellon <jtcp27031@gmail.com>
2018-12-15 21:19:12 -0500

    [feat]: created wireframes to the project and testing mails and aqlite

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using data;
+
+using System.Net.Mail;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*SmtpClient client = new SmtpClient();
+            client.Port = 587;
+            client.Host = "smtp.gmail.com";
+            client.EnableSsl = true;
+            client.Timeout = 10000;
+            client.DeliveryMethod = SmtpDeliveryMethod.Network;
+            client.UseDefaultCredentials = false;
+            client.Credentials = new System.Net.NetworkCredential("jcsanchezv1998@gmail.com", "Anta9804");
+
+            MailMessage mm = new MailMessage("jtcp27031@gmail.com", "antablack89@gmail.com", "tes1t", "test1");
+            mm.BodyEncoding = UTF8Encoding.UTF8;
+            mm.DeliveryNotificationOptions = DeliveryNotificationOptions.OnFailure;
+            client.Send(mm);*/
+            var connect = Connection.getInstance();
+            
+            //if (System.Diagnostics.Debugger.IsAttached) Console.ReadLine();
+        }
+    }
+}
---
+++ data/connection.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SQLite;
+
+namespace data
+{
+    public class Connection
+    {
+        public static SQLiteConnection connect = null;
+        public static SQLiteConnection getInstance() {
+            if (connect == null)
+            {
+                SQLiteConnection.CreateFile("SmartMailing.sqlite");
+                var m_dbConnection = new SQLiteConnection("Data Source=SmartMailing.sqlite;Version=3;");
+                m_dbConnection.Open();
+                return m_dbConnection;
+            }
+            else {
+                return connect;
+            }
+        }
+
+    }
+}
