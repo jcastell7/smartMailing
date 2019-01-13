8ac4abf74621218430aef6c685c6efb0bfa3187b
antablack <juan.sanchez@ultertechnologies.com>
2019-01-13 09:58:58 -0500

    Add create to contact form

---
+++ src/public/contacts.js
@@ -0,0 +1,24 @@
+import { create } from "../services/contacts";
+window.onload = function () {
+    let name = document.getElementById("name");
+    let email = document.getElementById("email");
+    let newBtn = document.getElementById("btn-new");
+    let saveBtn = document.getElementById("btn-save");
+    function clear() {
+        name.value = ""
+        email.value = ""
+    }
+    function save() {
+        if(name.value.trim() != "" && email.value.trim() != ""){
+            create(name.value, email.value).then(()=>{
+                alert("Guardado Exitoso");
+                clear();
+            }).catch((error)=>{
+                alert("Ha ocurrido un error");
+                console.error(error);
+            });
+        }
+    }
+    newBtn.addEventListener("click", clear);
+    saveBtn.addEventListener("click", save);
+}
---
+++ src/services/contacts.js
@@ -0,0 +1,15 @@
+import db from "./connection";
+
+export const create = (name, email) => {
+    return new Promise((done, reject) => {
+        var sql = ` INSERT INTO task_contacts (name, email) 
+        VALUES ('${name}', '${email}')`;
+        console.log(sql);
+        db.run(sql, {}, (error) => {
+            if (error) {
+                return reject(error)
+            }
+            done()
+        });
+    })
+}
