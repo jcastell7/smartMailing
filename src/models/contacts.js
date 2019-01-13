824a95d77970fdb3d0f423782b73418eee3312e3
antablack <juan.sanchez@ultertechnologies.com>
2019-01-13 11:09:50 -0500

    [feat]: Crud contact

--- src/models/contacts.js
+++ src/models/contacts.js
@@ -2,10 +2,10 @@ import db from "../services/connection";
 
 export const create = (name, email) => {
     return new Promise((done, reject) => {
-        var sql = ` INSERT INTO task_contacts (name, email) 
+        var query = ` INSERT INTO task_contacts (name, email) 
         VALUES ('${name}', '${email}')`;
-        console.log(sql);
-        db.run(sql, {}, (error) => {
+        console.log(query);
+        db.run(query, {}, (error) => {
             if (error) {
                 return reject(error)
             }
@@ -13,3 +13,31 @@ export const create = (name, email) => {
         });
     })
 }
+
+export const getContacts = () => {
+    return new Promise((done, reject) => {
+        let query = `SELECT * FROM task_contacts ORDER BY name ASC`;
+        db.all(query, {}, (error, result) => {
+            error ? reject(error) : done(result);
+        });
+    })
+}
+
+export const findById = _id => {
+    return new Promise((done, reject) => {
+        let query = `SELECT t.* FROM task_contacts t WHERE task_contact_id = ${_id}`;
+        db.get(query, {}, (error, result) => {
+            error ? reject(error) : done(result);
+        });
+    });
+};
+
+export const deleteById = _id => {
+    return new Promise((done, reject) => {
+        let query = `DELETE FROM task_contacts WHERE task_contact_id = ${_id}`;
+        db.run(query, {}, (error) => {
+            error ? reject(error) : done();
+        });
+    });
+}
+
--- src/public/contacts.js
+++ src/public/contacts.js
@@ -1,24 +1,68 @@
-import { create } from "../models/contacts";
+import * as contacts from "../models/contacts";
+var txtId = document.getElementById("id")
+var txtName = document.getElementById("name");
+var txtEmail = document.getElementById("email");
+var newBtn = document.getElementById("btn-new");
+var saveBtn = document.getElementById("btn-save");
+var btnDelete = document.getElementById("btn-delete");
+var list = document.getElementById("list");
 window.onload = function () {
-    let name = document.getElementById("name");
-    let email = document.getElementById("email");
-    let newBtn = document.getElementById("btn-new");
-    let saveBtn = document.getElementById("btn-save");
+
     function clear() {
-        name.value = ""
-        email.value = ""
+        txtId.value = "";
+        txtName.value = "";
+        txtEmail.value = "";
+        saveBtn.innerText = "Guardar";
+        btnDelete.style.display = "none";
     }
     function save() {
-        if(name.value.trim() != "" && email.value.trim() != ""){
-            create(name.value, email.value).then(()=>{
-                alert("Guardado Exitoso");
-                clear();
-            }).catch((error)=>{
-                alert("Ha ocurrido un error");
-                console.error(error);
-            });
+        if (txtName.value.trim() != "" && txtEmail.value.trim() != "") {
+            if (saveBtn.innerText == "Editar") {
+
+            } else {
+                contacts.create(txtName.value, txtEmail.value).then(() => {
+                    alert("Guardado Exitoso");
+                    clear();
+                    loadContacts()
+                }).catch((error) => {
+                    alert("Ha ocurrido un error");
+                    console.error(error);
+                });
+            }
         }
     }
+    function loadContacts() {
+        contacts.getContacts().then((data) => {
+            console.log(data)
+            list.innerHTML = '';
+            for (let item in data) {
+                console.log(item)
+                list.innerHTML += `<li class="list-group-item" onclick="selectItem(${data[item].task_contact_id})">${data[item].name}</li>`;
+            }
+        }).catch((error) => {
+            alert("Ha ocurrido un error");
+            console.error(error);
+        });
+    }
+    function deleteContact(){
+        if(confirm("¿ Estas Seguro ?")){
+            contacts.deleteById(txtId.value).then(()=>{
+                clear();    
+                loadContacts();
+            })
+        }
+    }
+    loadContacts()
     newBtn.addEventListener("click", clear);
     saveBtn.addEventListener("click", save);
+    btnDelete.addEventListener("click", deleteContact);
+}
+function selectItem(_id) {
+    contacts.findById(_id).then((item) => {
+        id.value = item.task_contact_id;
+        txtName.value = item.name;
+        txtEmail.value = item.email;
+        saveBtn.innerText = "Editar";
+        btnDelete.style.display = "block";
+    })
 }
