3755a33720397b343060fee5b4280a7e67bdd478
juan tomas castellon <jtcp27031@gmail.com>
2019-01-13 10:14:14 -0500

    [feat]: created products crud

--- src/models/products.js
+++ src/models/products.js
@@ -1,21 +1,29 @@
 import connection from "../services/connection";
 
-function createProduct(productName, quantity) {
-  let query = `INSERT INTO "task_products" ("name", "quantity") VALUES ('?', '?')`;
-  connection.db.run(query, [productName, quantity]);
-}
+export const createProduct = (productName, quantity) => {
+  return new Promise((done, reject) => {
+    let query = `INSERT INTO "task_products" ("name", "quantity") VALUES ('?', '?')`;
+    connection.db.run(query, [productName, quantity], error => {
+      error ? reject(error) : done();
+    });
+  });
+};
 
-function findById(_id) {
-  let query = `SELECT t.* FROM task_products t
+export const findById = _id => {
+  return new Promise((done, reject) => {
+    let query = `SELECT t.* FROM task_products t
     WHERE task_product_id = "?"`;
-  connection.db.run(query, [_id]);
-}
+    connection.db.run(query, [_id], error => {
+      error ? reject(error) : done();
+    });
+  });
+};
 
-function deleteById(_id) {
-  let query = `DELETE FROM "task_products" WHERE "task_product_id" = ?`;
-  connection.db.run(query, [_id]);
-}
-
-export default createProduct;
-export default findById;
-export default deleteById;
+export const deleteById = _id => {
+  return new Promise((done, reject) => {
+    let query = `DELETE FROM "task_products" WHERE "task_product_id" = ?`;
+    connection.db.run(query, [_id], error => {
+      error ? reject(error) : done();
+    });
+  });
+};
